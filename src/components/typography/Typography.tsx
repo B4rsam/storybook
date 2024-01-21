efd3f73aa610683dcbfc2602823b30b7468f5250
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-01-21 14:13:27 +0330

    feat: added font weights (broken)

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -2,14 +2,16 @@ import { FC } from 'react';
 import './typography.sass';
 
 type textVariant= "header1" | "header2" | "header3" | "subtitle1" | "subtitle2" | "body1" | "body2";
+type validWeights = 400 | 500 | 600 | 700 | null;
 interface IText extends HTMLParagraphElement {
     textType: textVariant;
+    fontWeight: validWeights;
 }
 
-const Typography: FC<Partial<IText>> = ({textType, children, ...other}) => {
+const Typography: FC<Partial<IText>> = ({textType,  fontWeight, children, ...other}) => {
 
     return (
-        <p {...other} className={textType} >{children}</p>
+        <p {...other} className={`${textType}${fontWeight ? ` fw-${fontWeight}` : ``}`} >{children}</p>
     )
 }
 
--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -12,48 +12,55 @@ type Story = StoryObj<typeof meta>;
 export const Header1 : Story = {
     args: {
         textType: "header1",
-        children: "Header 1"
+        children: "Header 1",
+        fontWeight: 400
     }
 }
 
 export const Header2 : Story = {
     args: {
         textType: "header2",
-        children: "Header 2"
+        children: "Header 2",
+        fontWeight: 400
     }
 }
 
 export const Header3 : Story = {
     args: {
         textType: "header3",
-        children: "Header 3"
+        children: "Header 3",
+        fontWeight: 400
     }
 }
 
 export const Subtitle1 : Story = {
     args: {
         textType: "subtitle1",
-        children: "Subtitle 1"
+        children: "Subtitle 1",
+        fontWeight: 400
     }
 }
 
 export const Subtitle2 : Story = {
     args: {
         textType: "subtitle2",
-        children: "Subtitle 2"
+        children: "Subtitle 2",
+        fontWeight: 400
     }
 }
 
 export const body1 : Story = {
     args: {
         textType: "body1",
-        children: "Body 1"
+        children: "Body 1",
+        fontWeight: 400
     }
 }
 
 export const body2 : Story = {
     args: {
         textType: "body2",
-        children: "Body 2"
+        children: "Body 2",
+        fontWeight: 400
     }
 }
