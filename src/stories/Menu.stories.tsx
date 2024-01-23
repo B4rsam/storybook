fbae8288974aef653c0840e3f126ba7e32cb6535
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-01-23 11:28:07 +0330

    feat: added menu stories

---
+++ src/stories/Menu.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MenuBar from '../components/navMenu/Menu.tsx';
+
+const meta = {
+    title: "Menu Bar",
+    component: MenuBar,
+} satisfies Meta<typeof MenuBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const NavigationBar : Story = {
+    args: {
+        logoType: "light",
+        logoText: "Logo",
+
+        btntype1: "tertiary",
+        btntype2: "tertiary",
+        btntype3: "tertiary",
+        btntype4: "tertiary",
+        btntype5: "primary",
+
+        btntext1: "Click me",
+        btntext2: "Click me",
+        btntext3: "Click me",
+        btntext4: "Click me",
+        btntext5: "Im Important!",
+
+        btnSizes: "medium",
+    }
+}
