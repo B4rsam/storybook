d00c02d7d15b42faf2901262304ceea7bb8b7950
mohsen <mbp.moh3n@gmail.com>
2024-01-27 16:09:44 +0330

    fix: alias

--- src/stories/Radio.stories.tsx
+++ src/stories/Radio.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Radio from '../components/radio/Radio';
+import Radio from "@/components/radio/Radio";
 
 const meta = {
     title: "Radio Buttons",
