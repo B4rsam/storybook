99b418a9a3e293b7c9af8d2a80e7b9e70d8df72c
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-01-27 15:28:49 +0330

    feat: added banners

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,42 @@
+import { FC, HTMLAttributes, ReactNode } from "react";
+import Typography from "../typography/Typography";
+import Icon from "../icon/Icon";
+import Button from "../button/Button";
+import './banner.sass';
+
+type bannerTypes = "info" | "success" | "alert";
+interface IBanner extends HTMLAttributes<HTMLDivElement> {
+    bannerType: bannerTypes;
+    bannerMessage: string;
+}
+
+const Banner : FC<IBanner> = ({bannerType, bannerMessage}) => {
+    const handleType = () => {
+        switch(bannerType) {
+            case "info":
+                return( 
+                    <div className="banner infoBanner">
+                        <Icon size="m" icon="info" />
+                        <Typography textType="body1" fontStyle="normal" fontWeight={400} children={bannerMessage}/>
+                        <Button btntype="tertiary" size="medium" classname="closebtn" children={<Icon size="m" icon="close"/>}/>
+                    </div> )
+            case "alert":
+                return( 
+                    <div className="banner alertBanner">
+                        <Icon size="m" icon="warning"/>
+                        <Typography textType="body1" fontStyle="normal" fontWeight={400} children={bannerMessage}/>
+                        <Button btntype="tertiary" size="medium" classname="closebtn" children={<Icon size="m" icon="close"/>}/>
+                    </div> )
+            case "success":
+                return( 
+                    <div className="banner successBanner">
+                        <Icon size="m" icon="check"/>
+                        <Typography textType="body1" fontStyle="normal" fontWeight={400} children={bannerMessage}/>
+                        <Button btntype="tertiary" size="medium" classname="closebtn" children={<Icon size="m" icon="close"/>}/>
+                    </div> )
+        }
+    }
+    return (handleType())
+}
+
+export default Banner;
---
+++ src/stories/Banner.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Banner from '../components/banner/Banner';
+
+const meta = {
+    title: "Banner",
+    component: Banner,
+} satisfies Meta<typeof Banner>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const InfoBanner : Story = {
+    args: {
+        bannerMessage: "Banner Message",
+        bannerType: "info"
+    }
+}
+
+export const AlertBanner : Story = {
+    args: {
+        bannerMessage: "Banner Message",
+        bannerType: "alert"
+    }
+}
+
+export const SuccessBanner : Story = {
+    args: {
+        bannerMessage: "Banner Message",
+        bannerType: "success"
+    }
+}
