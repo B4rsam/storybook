2d2c38d80c3bd3f45ba9e3ee5fe93c309095d5ee
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-01-31 12:45:54 +0330

    chore: checkmark sass module

--- src/components/checkmark/Checkmark.tsx
+++ src/components/checkmark/Checkmark.tsx
@@ -1,7 +1,5 @@
 import { FC, HTMLAttributes} from "react";
-import Icon from "../icon/Icon";
-import check from "../../assets/checkmark/check-svgrepo-com-2.svg"
-import './checkmark.sass';
+import s from './checkmark.module.sass';
 
 type checkSize = "small" | "medium";
 interface ICheck extends HTMLAttributes<HTMLInputElement> {
@@ -11,9 +9,9 @@ interface ICheck extends HTMLAttributes<HTMLInputElement> {
 
 const Checkmark : FC<Partial<ICheck>> = ({boxSize, id, ...other}) => {
     return (
-        <label className={`checkboxContainer ${boxSize}`}>
+        <label className={`${s.checkboxContainer} ${s[boxSize]}`}>
             <input id={id} type="checkbox" {...other} className={`checkbox`}/>
-            <div className={`checkboxControl ${boxSize}`}/>
+            <div className={`${s.checkboxControl} ${s[boxSize]}`}/>
         </label>
         
     )
