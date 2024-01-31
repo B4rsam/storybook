10f12261448e77e3caed6f1af4529641fb3b8c20
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-01-31 12:41:11 +0330

    chore: forms sass module

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,6 +1,6 @@
 import { FC, HTMLAttributes } from "react";
 import Icon from "../icon/Icon";
-import './form.sass';
+import s from './form.module.sass';
 
 type inputTypes = "input" | "selection";
 type shapes = "pill" | "normal";
@@ -20,28 +20,28 @@ const Form : FC<IInput> = ({inputType, placeHolder, shape, icon, className, ...o
                     switch(icon) {
                         case "search":
                             return (
-                                <div className={`field input ${shape} ${className}`}>
-                                    <i><Icon className="icon" size="s" icon="search"/></i>
+                                <div className={`input ${s.field} ${s[shape]} ${className}`}>
+                                    <i><Icon className={s.icon} size="s" icon="search"/></i>
                                     <input type="text" placeholder={placeHolder} />
                                 </div>
                             )
                         case "eye":
                             return (
-                                <div className={`field input ${shape} ${className}`}>
-                                    <i><Icon className="icon" size="s" icon="eye"/></i>
+                                <div className={`input ${s.field} ${s[shape]} ${className}`}>
+                                    <i><Icon className={s.icon} size="s" icon="eye"/></i>
                                     <input type="text" placeholder={placeHolder} />
                                 </div>
                             )
                         case "none":
                             return (
-                                <div className={`field input ${shape} ${className}`}>
+                                <div className={`input ${s.field} ${s[shape]} ${className}`}>
                                     <input type="text" placeholder={placeHolder} />
                                 </div>
                             )
                     } 
             case "selection":
                 return ( 
-                <select className={`field selection ${shape} ${className}`}>
+                <select className={`selection ${s.field} ${s[shape]} ${className}`}>
                     <option>Option 1</option>
                     <option>Option 2</option>
                     <option>Option 3</option>
